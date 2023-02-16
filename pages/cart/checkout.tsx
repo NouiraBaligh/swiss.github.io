95746073a4a7f7cbfef6b6ae6373a4cf0150711d
Nouira Baligh <nouirabaligh009@gmail.com>
2023-02-16 22:02:03 +0100

    Initial commit

---
+++ pages/cart/checkout.tsx
@@ -0,0 +1,143 @@
+import Layout from '../../layouts/Main';
+import { useSelector } from 'react-redux';
+import CheckoutStatus from '../../components/checkout-status';
+import CheckoutItems from '../../components/checkout/items';
+import { PayPalScriptProvider,PayPalButtons } from '@paypal/react-paypal-js';
+import { RootState } from 'store';
+
+const CheckoutPage = () => {
+
+  const priceTotal = useSelector((state: RootState) => {
+    const cartItems = state.cart.cartItems;
+    let totalPrice = 0;
+    if(cartItems.length > 0) {
+      cartItems.map(item => totalPrice += item.price * item.count);
+    }
+
+    return totalPrice;
+  })
+ 
+ 
+  return (
+    <Layout>
+         <section className="cart">
+        <div className="container">
+          <div className="cart__intro">
+            <h3 className="cart__title">Shipping and Payment</h3>
+            <CheckoutStatus step="checkout" />
+          </div>
+
+          <div className="checkout-content">
+            <div className="checkout__col-6">
+              <div className="checkout__btns">
+                <button className="btn btn--rounded btn--yellow">Log in</button>
+                <button className="btn btn--rounded btn--border">Sign up</button>
+              </div>
+
+              <div className="block">
+                <h3 className="block__title">Shipping information</h3>
+                <form className="form">
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input className="form__input form__input--sm" type="text" placeholder="Email" />
+                    </div>
+
+                    <div className="form__col">
+                      <input className="form__input form__input--sm" type="text" placeholder="Address" />
+                    </div>
+                  </div>
+                  
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input className="form__input form__input--sm" type="text" placeholder="First name" />
+                    </div>
+
+                    <div className="form__col">
+                      <input className="form__input form__input--sm" type="text" placeholder="City" />
+                    </div>
+                  </div>
+                  
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input className="form__input form__input--sm" type="text" placeholder="Last name" />
+                    </div>
+
+                    <div className="form__col">
+                      <input className="form__input form__input--sm" type="text" placeholder="Postal code / ZIP" />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input className="form__input form__input--sm" type="text" placeholder="Phone number" />
+                    </div>
+
+                    <div className="form__col">
+                      <div className="select-wrapper select-form">
+                        <select>
+                          <option>Country</option>
+                          <option value="Argentina">Argentina</option>  <option value="France">France</option>  <option value="England">England</option>
+                          <option value="Suisse">Suisse</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            
+            <div className="checkout__col-4">
+              <div className="block">
+                <h3 className="block__title">Payment method</h3>
+                <ul className="round-options round-options--three">
+                <PayPalScriptProvider options={{"client-id":"AZqoesjbE7XnmdZ_wQlmmFgxmwa7bNbGDS2YbmEjncjX0oMlOxCb-KpWBtLPZTLxQWbOA0M7AzoiWCmq"}}>
+                <PayPalButtons createOrder={(_data,actions)=>{
+                  return actions.order.create({
+                    purchase_units:[
+                      {amount:{
+                        value:`${priceTotal}`,
+                      },
+                    },
+                    ],
+                  });
+                }}
+              
+                ></PayPalButtons>
+                  </PayPalScriptProvider> 
+                
+                </ul>
+              </div>
+            
+            </div>
+            
+            <div className="checkout__col-2">
+              <div className="block">
+                <h3 className="block__title">Your cart</h3>
+                <CheckoutItems />
+                
+                <div className="checkout-total">
+                  <p>Total cost</p>
+                  <h3>${priceTotal}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className="cart-actions cart-actions--checkout">
+            <a href="/cart" className="cart__btn-back"><i className="icon-left"></i> Back</a>
+            <div className="cart-actions__items-wrapper">
+              {/* <button type="button" className="btn btn--rounded btn--border">Continue shopping</button>
+              <button type="button" className="btn btn--rounded btn--yellow">Proceed to payment</button>  
+                        */}
+            </div>
+          </div>
+        </div>
+      </section>
+    
+    </Layout>
+    
+  )
+};
+
+  
+export default CheckoutPage
---
+++ utils/data/products.ts
@@ -0,0 +1,350 @@
+export const products = [
+  {
+    id: '1',
+    name: 'Create an Android/IOS mobile app in Flutter ',
+    price: 255.00,
+    discount: 30,
+    quantityAvailable: 2,
+   
+    currentPrice: 255.00,
+  
+    images: [
+      '/images/products/product-1.jpg',
+    ],
+    punctuation: {
+      countOpinions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'William ',
+        avatar: '/images/12211.jpeg',
+        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
+        punctuation: 2
+      },
+      {
+        name: '   Madison',
+        avatar: '/images/443.png',
+        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
+        punctuation: 4
+      },
+      {
+        name: '     Isabella ',
+        avatar: '/images/213.jpeg',
+        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
+        punctuation: 5
+      },
+    ]
+  },
+  {
+    id: '2',
+    name: 'development of a mobile application (hybrid) with flutter',
+    price: 555.99,
+    quantityAvailable: 2,
+
+    currentPrice: 555,
+
+    images: [
+      '/images/products/product-2.jpg',
+    ],
+    punctuation: {
+      countOpinions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'William ',
+        avatar: '/images/12211.jpeg',
+        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
+        punctuation: 2
+      },
+      {
+        name: '   Madison',
+        avatar: '/images/443.png',
+        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
+        punctuation: 4
+      },
+      {
+        name: '     Isabella ',
+        avatar: '/images/213.jpeg',
+        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
+        punctuation: 5
+      },
+    ]
+  },
+  {
+    id: '3',
+    name: 'development of customs sections and pages in a Shopify theme',
+    price: 355,
+    quantityAvailable: 2,
+   
+    currentPrice: 355,
+ 
+    images: [
+      '/images/products/product-3.jpg',
+    ],
+    punctuation: {
+      countOpinions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'William ',
+        avatar: '/images/12211.jpeg',
+        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
+        punctuation: 2
+      },
+      {
+        name: 'Madison',
+        avatar: '/images/443.png',
+        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
+        punctuation: 4
+      },
+      {
+        name: 'Isabella ',
+        avatar: '/images/213.jpeg',
+        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
+        punctuation: 5
+      },
+    ]
+  },
+  {
+    id: '4',
+    name: 'create a multi products Shopify store',
+    price: 655,
+    quantityAvailable: 2,
+    
+    currentPrice: 655,
+    
+    images: [
+      '/images/products/product-4.jpg',
+    ],
+    punctuation: {
+      countOpinions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'William ',
+        avatar: '/images/12211.jpeg',
+        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
+        punctuation: 2
+      },
+      {
+        name: '   Madison',
+        avatar: '/images/443.png',
+        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
+        punctuation: 4
+      },
+      {
+        name: '     Isabella ',
+        avatar: '/images/213.jpeg',
+        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
+        punctuation: 5
+      },
+    ]
+  },
+  {
+    id: '5',
+    name: 'Analyze data in Excel, google Sheets or Accesssss',
+    price: 755.00,
+   
+    category: 'T-shirt',
+    currentPrice: 755.00,
+   
+    images: [
+      '/images/products/product-5.jpg',
+    ],
+    punctuation: {
+      countOpinions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'William ',
+        avatar: '/images/12211.jpeg',
+        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
+        punctuation: 2
+      },
+      {
+        name: '   Madison',
+        avatar: '/images/443.png',
+        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
+        punctuation: 4
+      },
+      {
+        name: '     Isabella ',
+        avatar: '/images/213.jpeg',
+        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
+        punctuation: 5
+      },
+    ]
+  },
+  {
+    id: '6',
+    name: 'Create an Android & IOS ecommerce mobile application',
+    price: 455,
+   currentPrice: 455,
+   images: [
+      '/images/products/product-6.jpg',
+    ],
+    punctuation: {
+      countOpinions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'William ',
+        avatar: '/images/12211.jpeg',
+        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
+        punctuation: 2
+      },
+      {
+        name: '   Madison',
+        avatar: '/images/443.png',
+        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
+        punctuation: 4
+      },
+      {
+        name: '     Isabella ',
+        avatar: '/images/213.jpeg',
+        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
+        punctuation: 5
+      },
+    ]
+  }, 
+];
+
+export default products;
