d38e2063a82481b83fc0dc3ac8e81b503641a9b8
Nouira Baligh <nouirabaligh009@gmail.com>
2023-02-17 22:39:28 +0100

    rf

--- pages/api/product/[pid].ts
+++ pages/api/product/[pid].ts
@@ -1,16 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-// fake data
+import { NextApiRequest, NextApiResponse } from 'next';
 import products from '../../../utils/data/products';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const {
     query: { pid },
-  } = req
+  } = req;
 
-  const product = products.find(x => x.id === pid);
-  res.setHeader("Cache-Control", "no-cache, must-revalidate, max-age=0");
-  res.setHeader("Vercel-Cache-Control", "s-maxage=1, stale-while-revalidate");
+  const product = products.find((x) => x.id === pid);
 
+  if (!product) {
+    res.status(404).json({ message: 'Product not found' });
+    return;
+  }
+
+  res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=30');
   res.status(200).json(product);
-}
+};
--- pages/product/[pid].tsx
+++ pages/product/[pid].tsx
@@ -1,5 +1,4 @@
-import { GetServerSideProps } from 'next'
-
+import { GetServerSideProps } from 'next';
 import { useState } from 'react';
 import Footer from '../../components/footer';
 import Layout from '../../layouts/Main';
@@ -9,17 +8,22 @@ import Gallery from '../../components/product-single/gallery';
 import Content from '../../components/product-single/content';
 import Description from '../../components/product-single/description';
 import Reviews from '../../components/product-single/reviews';
-import { server } from '../../utils/server'; 
-
-// types
+import { server } from '../../utils/server';
 import { ProductType } from 'types';
 
 type ProductPageType = {
   product: ProductType;
-}
+};
 
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const pid = query.pid;
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const pid = params?.pid;
+
+  if (!pid) {
+    return {
+      notFound: true
+    }
+  }
+
   const res = await fetch(`${server}/api/product/${pid}?t=${Date.now()}`);
   const product = await res.json();
 
@@ -36,32 +40,44 @@ const Product = ({ product }: ProductPageType) => {
   return (
     <Layout>
       <Breadcrumb />
- 
       <section className="product-single">
         <div className="container">
           <div className="product-single__content">
             <Gallery images={product.images} />
             <Content product={product} />
           </div>
-  
           <div className="product-single__info">
             <div className="product-single__info-btns">
-              <button type="button" onClick={() => setShowBlock('description')} className={`btn btn--rounded ${showBlock === 'description' ? 'btn--active' : ''}`}>Description</button>
-              <button type="button" onClick={() => setShowBlock('reviews')} className={`btn btn--rounded ${showBlock === 'reviews' ? 'btn--active' : ''}`}>Reviews (3)</button>
+              <button
+                type="button"
+                onClick={() => setShowBlock('description')}
+                className={`btn btn--rounded ${
+                  showBlock === 'description' ? 'btn--active' : ''
+                }`}
+              >
+                Description
+              </button>
+              <button
+                type="button"
+                onClick={() => setShowBlock('reviews')}
+                className={`btn btn--rounded ${
+                  showBlock === 'reviews' ? 'btn--active' : ''
+                }`}
+              >
+                Reviews (3)
+              </button>
             </div>
-
             <Description show={showBlock === 'description'} />
             <Reviews product={product} show={showBlock === 'reviews'} />
           </div>
         </div>
       </section>
-
       <div className="product-single-page">
         <ProductsFeatured />
       </div>
       <Footer />
     </Layout>
   );
-}
+};
 
-export default Product
+export default Product;
